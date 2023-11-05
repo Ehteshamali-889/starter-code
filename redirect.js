4bddd5244b31fda02b434c936ef29b7f78282ea6
CodeMan889 <alehtesham34@gmail.com>
2023-11-05 20:20:02 +0500

    redirect js file added

---
+++ redirect.js
@@ -0,0 +1,23 @@
+function redirectToXX99IPage(){
+    window.location.href = 'product-xx99-mark-one-headphones.html';
+  }
+
+  function redirectToXX59Page(){
+    window.location.href = 'product-xx59-headphones.html';
+  }
+
+  function redirectToZX9Page(){
+    window.location.href = 'product-zx9-speaker.html';
+  }
+
+  function redirectToheadphonesPage(){
+    window.location.href = 'headphones.html';
+  }
+
+  function redirectTospeakerPage(){
+    window.location.href = 'speakers.html';
+  }
+
+  function redirectToearphonePage(){
+    window.location.href = 'earphones.html';
+  }
